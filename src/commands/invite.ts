54063044904c8f54c25bc3a96fa456bf0d9e0013
paijose <paijose1@outlook.com>
2021-04-18 22:02:25 +0000

    Update invite.ts

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -1,12 +1,8 @@
-import { botID } from "../../deps.ts";
 import { createCommand } from "../utils/helpers.ts";
 
 createCommand({
   name: `invite`,
   execute: function (message) {
-    // Replace the permission number at the end to request the permissions you wish to request. By default, this will request Admin perms.
-    message.reply(
-      `https://discordapp.com/oauth2/authorize?client_id=${botID}&scope=bot&permissions=8`,
-    );
+    message.reply("https://discord.com/api/oauth2/authorize?client_id=830530080349749248&permissions=59392&scope=bot");
   },
 });
