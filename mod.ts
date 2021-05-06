2a1eb433ea552378c42ddb984ea0c8c20e8fe334
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-05-05 20:24:56 -0500

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,13 +1,12 @@
+import "https://deno.land/x/dotenv/load.ts";
 import { botCache, Intents, startBot } from "./deps.ts";
 import { configs } from "./configs.ts";
 import { fileLoader, importDirectory } from "./src/utils/helpers.ts";
-import { loadLanguages } from "./src/utils/i18next.ts";
 
 console.info(
-  "Beginning Bot Startup Process. This can take a little bit depending on your system. Loading now...",
+  "Beginning Bot Startup Process. This can take a little bit depending on your system. Loading now..."
 );
 
-// Forces deno to read all the files which will fill the commands/inhibitors cache etc.
 await Promise.all(
   [
     "./src/commands",
@@ -18,19 +17,12 @@ await Promise.all(
     "./src/tasks",
     "./src/permissionLevels",
     "./src/events",
-  ].map(
-    (path) => importDirectory(Deno.realPathSync(path)),
-  ),
+  ].map((path) => importDirectory(Deno.realPathSync(path)))
 );
 await fileLoader();
-//await loadLanguages();
-//await import("./src/database/database.ts");
 
 startBot({
   token: configs.token,
-  // Pick the intents you wish to have for your bot.
-  // For instance, to work with guild message reactions, you will have to pass the Intents.GUILD_MESSAGE_REACTIONS intent to the array.
   intents: [Intents.GUILDS, Intents.GUILD_MESSAGES],
-  // These are all your event handler functions. Imported from the events folder
   eventHandlers: botCache.eventHandlers,
 });
