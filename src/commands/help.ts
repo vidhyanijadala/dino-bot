52d5c79b26b4aa21b5e983ff923699258be369d0
pai <hachikopolarcito321@gmail.com>
2021-05-09 21:22:41 -0500

    i forget something

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -8,9 +8,14 @@ createCommand({
   execute: function (message) {
     const embed = new Embed()
       .setColor("random")
-        .setDescription(`\`\`\` help: \n *avatar: send you the avatar of the user \n *deno <code>: execute your code \n *invite:send you the invite of the bot \n *ping: send you a message to test the bot \n *stats: stats of the bot \`\`\``)
+      .setDescription(
+        `\`\`\` help: \n *avatar: send you the avatar of the user \n *deno <code>: execute your code \n *invite:send you the invite of the bot \n *ping: send you a message to test the bot \n *stats: stats of the bot \`\`\``
+      )
+      .setImage(
+        "https://media.discordapp.net/attachments/809999884820021252/841136200911683604/monda.gif"
+      )
       .setTimestamp();
 
-      return message.send({ embed });
+    return message.send({ embed });
   },
 });
