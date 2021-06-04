abb8d971d016e13ed776025ef833d721020f0313
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-06-04 09:28:06 -0500

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -9,7 +9,7 @@ createCommand({
     const embed = new Embed()
       .setColor("random")
       .setDescription(
-        `\`\`\` help: \n *avatar: send you the avatar of the user \n *deno <code>: execute your code \n *invite:send you the invite of the bot \n *ping: send you a message to test the bot \n *stats: stats of the bot \`\`\``
+        `\`\`\` help: \n *avatar: send you the avatar of the user \n *deno <code>: execute your code \n *invite:send you the invite of the bot \n *ping: send you a message to test the bot \n *stats: stats of the bot  \`\`\`[dino-bot source code](https://github.com/ELPanaJose/dino-bot)`
       )
       .setImage(
         "https://cdn.discordapp.com/attachments/849501077840986112/850354701102612490/unknown.png"
